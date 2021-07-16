3541102024981d8c40f9203cada3f9498dba0352
Brandon Hawi <brandonhawi@jasonbusiness.com>
2021-07-15 17:27:44 -0700

    Fix build action

--- src/Game.test.js
+++ src/Game.test.js
@@ -1,5 +1,5 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render } from '@testing-library/react';
+import { Game } from "./Game";
 
 test('renders main game', () => {
   render(<Game />);
