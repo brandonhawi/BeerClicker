837d62a9486fe492a07883e8f48c13cd0b0d5a0c
Brandon Hawi <brandonhawi@jasonbusiness.com>
2023-05-14 09:47:30 -0700

    More functional Achievement Display

--- src/entities/Achievements.tsx
+++ src/entities/Achievements.tsx
@@ -7,12 +7,43 @@ import {
   Typography,
   ImageList,
   ImageListItem,
+  Tooltip,
+  ImageListItemBar,
 } from "@mui/material";
 import EmojiEventsIcon from "@mui/icons-material/EmojiEvents";
 import { useState } from "react";
+import { Achievement } from "../types/achievement";
 
-export default function Achievements() {
+type Props = {
+  achievements?: {
+    achievementData: Map<string, Achievement>;
+  };
+};
+
+export default function Achievements({ achievements }: Props) {
   const [achievementsShown, setAchievementsShown] = useState(false);
+  if (!achievements) {
+    return null;
+  }
+  const { achievementData } = achievements;
+  const achievementsRender: JSX.Element[] = [];
+  achievementData.forEach(({ earned, name, description }) => {
+    if (earned) {
+      achievementsRender.push(
+        <Tooltip title={`${name}: ${description}`}>
+          <ImageListItem>
+            <img src="/beerenomics.png" alt="beerenomics achievements" />
+          </ImageListItem>
+        </Tooltip>
+      );
+    } else {
+      achievementsRender.push(
+        <ImageListItem>
+          <img src="https://placehold.co/50" alt="placeholder" />
+        </ImageListItem>
+      );
+    }
+  });
 
   return (
     <>
@@ -43,21 +74,13 @@ export default function Achievements() {
             top: "50%",
             left: "50%",
             transform: "translate(-50%, -50%)",
-            width: 400,
+            width: 600,
             padding: 2,
           }}
         >
           <Typography variant="h6">Achievements</Typography>
           <ImageList sx={{ width: 400, height: 450 }} cols={8} rowHeight={50}>
-            <ImageListItem>
-              <img src="/beerenomics.png" alt="beerenomics achievements" />
-            </ImageListItem>
-            <ImageListItem>
-              <img src="https://placehold.co/50" alt="placeholder" />
-            </ImageListItem>
-            <ImageListItem>
-              <img src="https://placehold.co/50" alt="placeholder" />
-            </ImageListItem>
+            {achievementsRender}
           </ImageList>
         </Paper>
       </Modal>
